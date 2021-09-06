d27be6bcbbb2a49692903d58e772d41ee465a20a
joshzwrd <joshzwrd@gmail.com>
2021-09-06 13:43:55 +0200

    update files

--- php/2_PHP_Oriente_Objet/Exercices_AVC1/Exercice_12.php
+++ php/2_PHP_Oriente_Objet/Exercices_AVC1/Exercice_12.php
@@ -1,14 +1,61 @@
 <?php
 
-interface vehicle
+interface transport
 {
 
     function accelerate();
     function slowDown();
 }
 
-class Car implements vehicle
+class Vehicule implements transport
 {
+    protected $name;
+    protected $color;
+    protected $speed;
+    protected $wheel;
+
+    function __construct($n, $c, $s,$w)
+    {
+        $this->name = $n;
+        $this->color = $c;
+        $this->speed = $s;
+        $this->wheel = $w;
+    }
+
+    // Getters/Setters
+
+    function getName(){
+        return $this->name;
+    }
+
+    function setName($n){
+        $this->name = $n; 
+    }
+
+    function getColor(){
+        return $this->color;
+    }
+
+    function setColor($c){
+        $this->color = $c; 
+    }
+
+    function getSpeed(){
+        return $this->speed;
+    }
+
+    function setSpeed($s){
+        $this->speed = $s; 
+    }
+
+    function getWheel(){
+        return $this->wheel;
+    }
+
+    function setWheel($w){
+        $this->wheel = $w; 
+    }
+
 
     function accelerate()
     {
@@ -20,8 +67,53 @@ class Car implements vehicle
     }
 }
 
-class Moto implements vehicle
+class Airplane implements transport
 {
+    protected $name;
+    protected $color;
+    protected $speed;
+    protected $capacity;
+
+    function __construct($n, $co, $s,$ca)
+    {
+        $this->name = $n;
+        $this->color = $co;
+        $this->speed = $s;
+        $this->wheel = $ca;
+    }
+
+    function getName(){
+        return $this->name;
+    }
+
+    function setName($n){
+        $this->name = $n; 
+    }
+
+    function getColor(){
+        return $this->color;
+    }
+
+    function setColor($co){
+        $this->color = $co; 
+    }
+
+    function getSpeed(){
+        return $this->speed;
+    }
+
+    function setSpeed($s){
+        $this->speed = $s; 
+    }
+
+    function getCapacity(){
+        return $this->capacity;
+    }
+
+    function setCapacity($ca){
+        $this->capacity = $ca; 
+    }
+
 
     function accelerate()
     {
@@ -32,3 +124,4 @@ class Moto implements vehicle
         $this->speed--;
     }
 }
+
